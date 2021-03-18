3a060f066e9a144d376c1cc7365bfeff682c9672
appithe <joelaviles413@gmail.com>
2021-03-17 23:38:12 -0600

    HomaPage added

---
+++ HomePage/js/index.js
@@ -0,0 +1,16 @@
+var item = document.getElementById("items");
+
+const exercise_link = (name, route, description, image) => {
+    let cardContent = `<div class="col-12 col-md-6 col-lg-4 col-xl-3"><div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;"><img src="${image}" class="card-img-top"><div class="card-body"><h5 class="card-title">${name}</h5><p class="card-text">${description}</p><a href="${route}" class="btn btn-primary">Ver Practica</a></div></div></div>`;
+    return cardContent;
+}
+
+for (var i = 0; i < routes.length; i++) {
+    var {
+        name,
+        route,
+        description,
+        image
+    } = routes[i];
+    item.innerHTML += exercise_link(name, route, description, image);
+}
---
+++ HomePage/js/routes.js
@@ -0,0 +1,53 @@
+const routes = [
+    {
+        name: "Practica 1",
+        route: "../Practica 1/index.html",
+        description: "Introduccion a bootstrap, uso de mapas y animaciones con AOS",
+        image: ""
+    }, {
+        name: "Practica 2",
+        route: "../Practica 2/index.html",
+        description: "Uso la funcion navigator para acceder a los datos del navagador",
+        image: ""
+    }, {
+        name: "Practica 3",
+        route: "../Practica 3/index.html",
+        description: "Uso de navigator para saber las coordenadas de un dispositovo",
+        image: ""
+    }, {
+        name: "Practica 4",
+        route: "../Practica 4/index.html",
+        description: "Uso del api staticmap para desplegar mapas estaticos",
+        image: ""
+    }, {
+        name: "Practica 5",
+        route: "../Practica 5/index.html",
+        description: "Uso del api JsMaps para desplegar mapas y agregar un marcador.",
+        image: ""
+    }, {
+        name: "Practica 6",
+        route: "../Practica 6/index.html",
+        description: "Uso del api JsMaps para desplegar mapas",
+        image: ""
+    }, {
+        name: "Practica 7",
+        route: "../Practica 7/index.html",
+        description: "Uso de fetch para consultar los casos de covid-19 de los algunos paises desde un API",
+        image: ""
+    }, {
+        name: "Practica 8",
+        route: "../Practica 8/index.html",
+        description: "Uso de fetch para consultar los casos y muertes por covid-19 de todos los paisesdesde un API",
+        image: ""
+    }, {
+        name: "Practica 9",
+        route: "../Practica 9/index.html",
+        description: "Poner marcadores a paises",
+        image: ""
+    }, {
+        name: "Practica 10",
+        route: "../Practica 10/index.html",
+        description: "Mapa de casos de covid-19 en los paises",
+        image: ""
+    },
+]
