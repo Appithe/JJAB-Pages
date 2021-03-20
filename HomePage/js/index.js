6cb13c47ca468e3ddc29bdb44fb963768ef2bed3
appithe <joelaviles413@gmail.com>
2021-03-19 20:26:13 -0600

    Practica 12

--- HomePage/js/index.js
+++ HomePage/js/index.js
@@ -1,7 +1,7 @@
 var item = document.getElementById("items");
 
 const exercise_link = (name, route, description, image) => {
-    let cardContent = `<div class="col-12 col-md-6 col-lg-4 col-xl-3"><div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;"><img src="${image}" class="card-img-top"><div class="card-body"><h5 class="card-title">${name}</h5><p class="card-text">${description}</p><a href="${route}" class="btn btn-primary">Ver Practica</a></div></div></div>`;
+    let cardContent = `<div class="col-12 col-md-6 col-lg-4 col-xl-3"><div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;"><div class="card-body"><h5 class="card-title">${name}</h5><p class="card-text">${description}</p><a href="${route}" class="btn btn-primary">Ver Practica</a></div></div></div>`;
     return cardContent;
 }
 
--- HomePage/js/routes.js
+++ HomePage/js/routes.js
@@ -2,57 +2,50 @@ const routes = [
     {
         name: "Practica 1",
         route: "../Practica 1/index.html",
-        description: "Introduccion a bootstrap, uso de mapas y animaciones con AOS",
-        image: ""
+        description: "Introduccion a bootstrap, uso de mapas y animaciones con AOS"
     }, {
         name: "Practica 2",
         route: "../Practica 2/index.html",
-        description: "Uso la funcion navigator para acceder a los datos del navagador",
-        image: ""
+        description: "Uso la funcion navigator para acceder a los datos del navagador"
     }, {
         name: "Practica 3",
         route: "../Practica 3/index.html",
-        description: "Uso de navigator para saber las coordenadas de un dispositovo",
-        image: ""
+        description: "Uso de navigator para saber las coordenadas de un dispositovo"
     }, {
         name: "Practica 4",
         route: "../Practica 4/index.html",
-        description: "Uso del api staticmap para desplegar mapas estaticos",
-        image: ""
+        description: "Uso del api staticmap para desplegar mapas estaticos"
     }, {
         name: "Practica 5",
         route: "../Practica 5/index.html",
-        description: "Uso del api JsMaps para desplegar mapas y agregar un marcador.",
-        image: ""
+        description: "Uso del api JsMaps para desplegar mapas y agregar un marcador."
     }, {
         name: "Practica 6",
         route: "../Practica 6/index.html",
-        description: "Uso del api JsMaps para desplegar mapas",
-        image: ""
+        description: "Uso del api JsMaps para desplegar mapas"
     }, {
         name: "Practica 7",
         route: "../Practica 7/index.html",
-        description: "Uso de fetch para consultar los casos de covid-19 de los algunos paises desde un API",
-        image: ""
+        description: "Uso de fetch para consultar los casos de covid-19 de los algunos paises desde un API"
     }, {
         name: "Practica 8",
         route: "../Practica 8/index.html",
-        description: "Uso de fetch para consultar los casos y muertes por covid-19 de todos los paisesdesde un API",
-        image: ""
+        description: "Uso de fetch para consultar los casos y muertes por covid-19 de todos los paisesdesde un API"
     }, {
         name: "Practica 9",
         route: "../Practica 9/index.html",
-        description: "Poner marcadores a paises",
-        image: ""
+        description: "Poner marcadores a paises"
     }, {
         name: "Practica 10",
         route: "../Practica 10/index.html",
-        description: "Mapa de casos de covid-19 en los paises",
-        image: ""
+        description: "Mapa de casos de covid-19 en los paises"
     }, {
         name: "Practica 11",
         route: "../Practica 11/index.html",
-        description: "Uso de Clustering para agrupar los marcadores y cambio del tipo de vista en el mapa",
-        image: "./img/p11.png"
+        description: "Uso de Clustering para agrupar los marcadores y cambio del tipo de vista en el mapa"
+    }, {
+        name: "Practica 12",
+        route: "../Practica 12/index.html",
+        description: "Cambio de idioma a un mapa"
     },
 ]
