a536f46c3f25a8588036b8dca1236189d6688759
joel <joelaviles413@gmail.com>
2021-07-02 23:50:11 -0500

    practica 4 firebase

---
+++ Firebase/Practica 4/scripts/autenticacion.js
@@ -0,0 +1,123 @@
+auth.onAuthStateChanged(user => {
+    if (user) {
+
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition( position =>{
+                db.collection('usuarios').doc(user.uid).update({
+                    coordenadas : {
+                        latitude : position.coords.latitude,
+                        longitude : position.coords.longitude
+                    }
+                });
+            });
+        }
+
+        db.collection('platillos').onSnapshot(snapshot => {
+            cargarPlatillos(snapshot.docs);
+        });
+        configurarMenu(user);
+    } else {
+        cargarPlatillos([]);
+        configurarMenu();
+    }
+});
+
+const formaIngresar = document.getElementById('formaIngresar');
+
+formaIngresar.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let correo = formaIngresar['correo'].value;
+    let password = formaIngresar['pass'].value;
+
+    auth.signInWithEmailAndPassword(correo, password).then(cred => {
+        console.log(cred);
+
+        $('#IngresarModal').modal('hide');
+        formaIngresar.reset();
+        formaIngresar.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        formaIngresar.querySelector('.error').innerHTML = mensajeError(err.code);
+        console.log(err);
+    });
+});
+
+function mensajeError(code) {
+    let mensaje = '';
+
+    switch (code) {
+        case 'auth/wrong-password':
+            mensaje = 'Su contraseña no es correcta';
+            break;
+
+        case 'auth/user-not-found':
+            mensaje = 'Usuario no encontrado';
+            break;
+
+        case 'auth/weak-password':
+            mensaje = 'Contraseña debil';
+            break;
+        default:
+            mensaje = code;
+    }
+
+    return mensaje;
+}
+
+const salir = document.getElementById('salir');
+
+salir.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    auth.signOut().then(() => {
+        console.log('signOut');
+    });
+});
+
+const formaRegistro = document.getElementById('formaRegistro');
+
+formaRegistro.addEventListener('submit', (e) => {
+    e.preventDefault;
+
+    const correo = formaRegistro['rcorreo'].value;
+    const pass = formaRegistro['rpass'].value;
+
+    auth.createUserWithEmailAndPassword(correo, pass).then(cred => {
+        return db.collection('usuarios').doc(cred.user.uid).set({
+            nombre: formaRegistro['rnombre'].value,
+            telefono: formaRegistro['rtelefono'].value,
+            direccion: formaRegistro['rdireccion'].value
+        });
+    }).then(() => {
+        $('#RegistroModal').modal('hide');
+        formaRegistro.reset();
+        formaRegistro.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        formaRegistro.querySelector('.error').innerHTML = mensajeError(err.code);
+        console.log(err.code);
+    });
+});
+
+ingresarConGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signWithPopup(provider).then(result => {
+        var token = result.credential.accessToken;
+        console.log(token);
+
+        var user = result.user;
+
+        let html = `
+            <p>Nombre: ${user.displayName}</p>
+            <p>Correo: ${user.email}</p>
+            <img src="${user.photoURL}" width="50px">
+        `;
+
+        infoCuenta.innerHTML = html;
+        $('#IngresarModal').modal('hide');
+        formaIngresar.reset();
+        formaIngresar.querySelector('.error') = '';
+    }).catch(error => {
+        console.log(error);
+    });
+}
---
+++ Firebase/Practica 4/scripts/index.js
@@ -0,0 +1,81 @@
+const loggedout = document.querySelectorAll('.logged-out');
+const loggedin = document.querySelectorAll('.logged-in');
+const infoCuenta = document.querySelector('.infoCuenta');
+
+const configurarMenu = (user) => {
+    if (user) {
+
+        db.collection('usuarios').doc(user.uid).get().then(doc => {
+            const html = `
+                <p>Nombre: ${doc.data().nombre}</p>
+                <p>Correo: ${doc.data().correo}</p>
+                <p>Telefono: ${doc.data().telefono}</p>
+                <p>Direccion: ${doc.data().direccion}</p>
+            `;
+
+            infoCuenta.innerHTML = html;
+        });
+
+        loggedin.forEach(item => item.style.display = 'block');
+        loggedout.forEach(item => item.style.display = 'none');
+    } else {
+        loggedin.forEach(item => item.style.display = 'none');
+        loggedout.forEach(item => item.style.display = 'block');
+    }
+};
+
+const lstPlatillos = document.getElementById('lstPlatillos');
+
+const cargarPlatillos = (data) => {
+    if (data.legth) {
+
+        let html = '';
+
+        data.forEach(doc => {
+            const platillo = doc.data();
+            const columna = `
+                <div class="col-4 col-md-4">
+                    <img src="./imagenes/${platilo.imagen}" class="img-thumbnail" alt="${platillo.nombre}">
+                    <p>${platillo.nombre}</p>
+                    <p class="text-danger">$ ${platillo.precio}</p>
+                    <a href="https://www.paypal.me/grupohernandezalba/${platillo.precio}" target="_blank">
+                        <button class=""btn btn-primary">Pagar Ahora</button>
+                    </a>
+                </div>
+            `;
+
+            html += columna;
+        });
+
+        lstPlatillos.innerHTML = html;
+    } else {
+        lstPlatillos.innerHTML = "favor de ingresar";
+    }
+};
+
+const obtieneAmigos = (data) => {
+
+    var propiedades = {
+        center: {
+            lat: 21.152639, lng: -101.711598
+        },
+        zoom: 14
+    }
+
+    var mapa = document.getElementById("map")
+    var map = new google.maps.Map(mapa, propiedades);
+
+    data.forEach(doc => {
+
+        informacion = new google.maps.InfoWindow;
+
+        var pos = {
+            lat: doc.data().coordenadas.latitude,
+            lng: doc.data().coordenadas.longitude
+        };
+
+        informacion.setPosition(pos);
+        informacion.setContent(doc.data().nombre);
+        informacion.open(map);
+    });
+};
